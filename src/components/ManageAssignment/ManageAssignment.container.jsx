1d842d5f2d9472d3b9a96bf7aa1d90a397f68f15
Nguyen Xuan Tam <xuantam0304@gmail.com>
2020-06-13 01:05:12 +0700

    Add manage assignment component

---
+++ src/components/ManageAssignment/ManageAssignment.container.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable import/named */
+/* eslint-disable import/no-named-as-default */
+import { connect } from 'react-redux'
+import { getAllTag, createTag, editTag, deleteTag } from '../../redux/tag/tag.action'
+import { getAllMajor } from '../../redux/major/major.action'
+import ManagerTagSkill from './ManageAssignment.component'
+
+const mapStateToProps = state => ({
+  data: state.tag.data,
+  dataMajor: state.major.data,
+  loadingData: state.major.loading,
+  length: state.tag.length,
+})
+
+const mapDispatchToProps = dispatch => ({
+  getAllMajor: () => dispatch(getAllMajor()),
+  getAllTag: data => dispatch(getAllTag(data)),
+  createTag: data => dispatch(createTag(data)),
+  editTag: data => dispatch(editTag(data)),
+  deleteTag: data => dispatch(deleteTag(data)),
+})
+
+const ManagerTagSkillContainer = connect(mapStateToProps, mapDispatchToProps)(ManagerTagSkill)
+
+export default ManagerTagSkillContainer
